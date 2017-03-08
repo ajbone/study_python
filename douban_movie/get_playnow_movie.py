0b96cfde4629570e6b369df001c150bf98cf8872
ajbone <ajbone@sina.com>
2017-03-08 18:21:39 +0800

    add get nowplaying movie

---
+++ douban_movie/get_playnow_movie.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python  
+#coding: utf-8  
+
+import time
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://movie.douban.com/nowplaying/hangzhou/"
+
+try:
+	response = requests.request("GET", url)
+except Exception as e:
+    print('请求出现错误，错误信息：%s' %e)
+else:
+	soup = BeautifulSoup(response.text,"lxml")
+	#print soup.prettify()
+
+	movie_all = soup.find_all('div',{"id":"nowplaying"})
+	#print movie_all
+	print "杭州正在上映的电影："
+	for i in movie_all:
+		for j in i.find_all('li',{"class":"list-item"}):
+			print "电影名称:",j["data-title"]
+			print "豆瓣评分：",j["data-score"]
+
+
+'''
+soup = BeautifulSoup(response.text,"lxml")
+#print soup.prettify()
+
+#print soup.find_all('li',{"class":"list-item"})[0]['data-title']
+movie_all = soup.find_all('div',{"id":"nowplaying"})
+#print movie_all
+
+
+for i in movie_all:
+	for j in i.find_all('li',{"class":"list-item"}):
+		print "电影名称:",j["data-title"]
+		print "豆瓣评分：",j["data-score"]
+'''
+
